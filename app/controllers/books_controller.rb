fb344d1684ec60bafa2a2d0426e5b5c9f15e4690
Dan Ste. Marie Jr <DSte.mariejr@gmail.com>
2020-08-04 14:10:10 -0400

    WIP: Added validations for adding a new book to shelf.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,32 +11,33 @@ class BooksController < ApplicationController
     end
 
     post '/books' do
-      #add params validations
-        if params[:book][:title] != "" && params[:book][:author] != ""
-            @book = Book.find_or_create_by(params[:book])
+        flash[:info] = "Please enter a title and author."
+        @book = Book.find_or_create_by(params[:book])
+        if @book.valid?
             current_user.user_books.create(book_id: @book.id, pages_read: 0, read: false)
             redirect "/books"           
         else
-            redirect '/failure'
+            flash[:info]
+            redirect "/books/new"
         end
-
 
     end
 
     get '/books/:id' do
-        @book = Book.find_by_id(params[:id])
+        @book = Book.find_by_id(params[:id])       
         erb :"books/show"
     end
 
     patch '/books/:id' do
-        @book = current_user.user_books.find_by(book_id: params[:id])
-        @book.update(params[:bookmark])
+        @bookmark = current_user.user_books.find_by(book_id: params[:id])
+        @bookmark.update(params[:bookmark])
+        @book = Book.find_by_id(params[:id])
+        flash[:update] = "#{@book.title} has been updated!"
+        flash[:update]
         redirect "/books"        
     end
 
     delete '/books/:id' do
-        #deletefrom  user_books?
-        
         @book = current_user.books.find_by_id(params[:id])
         current_user.books.delete(@book)
         redirect "/books"
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,10 @@
 class Book < ActiveRecord::Base
     has_many :user_books
     has_many :users, through: :user_books
+    validates :title, presence: true
+    validates :author, presence: true
+    # validates_associated :user_books
+    # validates_associated :users
     # before_save :set_default_status
 
 
