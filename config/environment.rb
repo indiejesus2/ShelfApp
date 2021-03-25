6612823b4339c6631674cb70cfee25bd85fd279e
indiejesus2 <62215604+indiejesus2@users.noreply.github.com>
2021-03-25 19:53:17 -0400

    WIP: Updated environment.rb to upload database.

--- config/environment.rb
+++ config/environment.rb
@@ -13,7 +13,7 @@ configure :production do
   db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
 
   ActiveRecord::Base.establish_connection(
-    :adapter => db.scheme == 'postgres' ? 'postgresql' : db:scheme,
+    :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
     :host => db.host,
     :username => db.user,
     :password => db.password,
