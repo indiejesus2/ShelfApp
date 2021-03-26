d14aae361cc31fab0a02bc5e38245e1016cc2983
indiejesus2 <62215604+indiejesus2@users.noreply.github.com>
2021-03-25 20:55:04 -0400

    WIP: Removing Sinatra:Flash

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,6 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
     enable :sessions
     set :session_secret, ENV['SESSION_SECRET']
-    register Sinatra::Flash
   end
 
   get '/' do
